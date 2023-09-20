62208f407f0027ba19f85bdf091079e076e167af
Larrire <linekerlarrire@gmail.com>
2023-09-20 20:39:15 -0300

    Criando módulo de categorias

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 // const middlewares = require('./middlewares');
 
 const router = Router();
@@ -12,5 +13,11 @@ router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
+
+router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
+router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
+router.delete('/categories/:id', CategoryController.delete);
 
 module.exports = router;
